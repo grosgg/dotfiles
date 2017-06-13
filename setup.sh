07cab0cf574b0bbd2751e18e1cb1fbc65e70a97c
Jeremy Marin <j.marin@crowdcast.jp>
2017-06-13 23:38:41 +0900

    Change config for neovim

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ if [ ! \( -e ~/.zshrc \) ]; then
   ln -s ~/dotfiles/aliases ~/.aliases
   ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
   ln -s ~/dotfiles/vimrc ~/.vimrc
+  ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
   ln -s ~/dotfiles/vimrc.bundles ~/.vimrc.bundles
   ln -s ~/dotfiles/zshenv ~/.zshenv
   ln -s ~/dotfiles/zshrc ~/.zshrc
