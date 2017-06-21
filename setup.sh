ccf554cc068cc5aa3209617ad64ad0195101b492
Jeremy Marin <marin.jeremy@gmail.com>
2017-06-21 16:28:17 +0900

    Add nvim config folder creation

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if [ ! \( -e ~/.config.lock \) ]; then
   rm -f ~/.aliases ~/.tmux.conf ~/.vimrc ~/.config/nvim/init.vim ~/.vimrc.bundles ~/.zshrc
+  mkdir -p ~/.config/nvim
 
   ln -s ~/dotfiles/aliases ~/.aliases
   ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
